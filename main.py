b31b622b7e85f5cb74ce6867b04d52bb3d8a334f
Muhinhassan26 <muhinhassan123@gmail.com>
2026-01-27 18:40:58 +0600

    Ran a demo api just for testing

--- main.py
+++ main.py
@@ -1,6 +1,24 @@
-def main():
-    print("Hello from fastapi-blog!")
+from fastapi import FastAPI
+
+blog:list[dict]=[
+    {
+        "id":1,
+        "title":"Blog 1",
+        "content":"Content of blog 1"
+    },
+    {
+        "id":2,
+        "title":"Blog 2",
+        "content":"Content of blog 2"
+    }
+]
+
+app = FastAPI()
+
+@app.get("/blog")
+def get_blogs():
+    return {"data": blog}
 
 
-if __name__ == "__main__":
-    main()
+
+
